77b199b71214935a85f415c6adf651b18f8e3c46
yewenyu <289193866@qq.com>
2023-11-09 16:58:08 +0800

    libev and mbedtls

---
+++ shadowsoccks/libsodium/libsodium.h
@@ -0,0 +1,18 @@
+//
+//  libsodium.h
+//  libsodium
+//
+//  Created by 叶文宇 on 2023/11/9.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for libsodium.
+FOUNDATION_EXPORT double libsodiumVersionNumber;
+
+//! Project version string for libsodium.
+FOUNDATION_EXPORT const unsigned char libsodiumVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libsodium/PublicHeader.h>
+
+
