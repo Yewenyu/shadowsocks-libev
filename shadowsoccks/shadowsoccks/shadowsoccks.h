7ac3bbc039f1e5aae478eb0e185673c6e9627c9d
yewenyu <289193866@qq.com>
2023-11-09 12:01:43 +0800

    ios framework

---
+++ shadowsoccks/shadowsoccks/shadowsoccks.h
@@ -0,0 +1,18 @@
+//
+//  shadowsoccks.h
+//  shadowsoccks
+//
+//  Created by 叶文宇 on 2023/11/9.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for shadowsoccks.
+FOUNDATION_EXPORT double shadowsoccksVersionNumber;
+
+//! Project version string for shadowsoccks.
+FOUNDATION_EXPORT const unsigned char shadowsoccksVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <shadowsoccks/PublicHeader.h>
+
+
